9393ace4a1e6374cf9bf4da48495397cb0089c1c
Luis Eduardo Imbernón <leimbernon@ai-mind.com>
2019-12-01 17:33:53 +0100

    Added the schema and modified the properties.

--- src/main/java/artie/sensor/client/ClientApplication.java
+++ src/main/java/artie/sensor/client/ClientApplication.java
@@ -1,13 +1,30 @@
 package artie.sensor.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ClientApplication {
+import artie.sensor.client.model.Sensor;
+import artie.sensor.client.repository.SensorRepository;
 
+@SpringBootApplication
+public class ClientApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private SensorRepository sensorRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 
 }
