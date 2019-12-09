f3e80f2dbb18aef6263db35657b045a88f068719
Luis Eduardo Imbernón <leimbernon@ai-mind.com>
2019-12-09 12:58:48 +0100

    Modified the events, and the service read.

--- src/main/java/artie/sensor/client/event/GenericArtieEvent.java
+++ src/main/java/artie/sensor/client/event/GenericArtieEvent.java
@@ -1,19 +1,57 @@
 package artie.sensor.client.event;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationEvent;
+import artie.sensor.common.dto.SensorObject;
+
 
 public class GenericArtieEvent extends ApplicationEvent {
 	
 	private static final long serialVersionUID = 1L;
-	private String message;
+	protected String action;
+	protected String message;
+	protected String sensorName;
 	protected boolean success;
+	protected List<SensorObject> data;
 	
-	public GenericArtieEvent(Object source, String message, boolean success) {
+	public GenericArtieEvent(Object source, String action, String sensorName, boolean success){
+		super(source);
+		this.action = action;
+		this.sensorName = sensorName;
+		this.success = success;
+	}
+	
+	public GenericArtieEvent(Object source, String action, String sensorName, String message, boolean success) {
 		super(source);
 		this.message = message;
+		this.action = action;
+		this.sensorName = sensorName;
 		this.success = success;		
 	}
+	
+	public GenericArtieEvent(Object source, String action, String sensorName, List<SensorObject> data, boolean success){
+		super(source);
+		this.action = action;
+		this.sensorName = sensorName;
+		this.data = data;
+		this.success = success;
+	}
 
+	public String getAction(){
+		return this.action;
+	}
+	public void setAction(String action){
+		this.action = action;
+	}
+	
+	public String getSensorName(){
+		return this.sensorName;
+	}
+	public void setSensorName(String sensorName){
+		this.sensorName = sensorName;
+	}
+	
 	public String getMessage() {
 		return message;
 	}
@@ -28,6 +66,11 @@ public class GenericArtieEvent extends ApplicationEvent {
 		this.success = success;
 	}
 	
-	
+	public List<SensorObject> gatData(){
+		return this.data;
+	}
+	public void setData(List<SensorObject> data){
+		this.data = data;
+	}
 	
 }
